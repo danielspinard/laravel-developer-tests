a4e5ec9b13b464b846037981846ce9f094e09811
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 17:56:16 -0300

    Refactoring UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,21 +3,37 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
+    /**
+     * @const string
+     */
+    private const ADMIN_NAME = 'Daniel Spinardi';
+    
+    /**
+     * @const string
+     */
+    private const ADMIN_EMAIL = 'admin@admin.com';
+
+    /**
+     * @const string
+     */
+    private const ADMIN_PASSWORD = 'password';
+
     /**
      * @return void
      */
     public function run(): void
     {
         \App\Models\User::create([
-            'name' => 'System Admin',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'name' => UserSeeder::ADMIN_NAME,
+            'email' => UserSeeder::ADMIN_EMAIL,
+            'password' => Hash::make(UserSeeder::ADMIN_PASSWORD),
             'remember_token' => Str::random(10),
+            'email_verified_at' => now()
         ]);
     }
 }
