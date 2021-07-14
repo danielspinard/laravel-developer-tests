2404f7d76c7c8f8b634c856dce10d30a17461d67
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 21:35:48 -0300

    Insert EmployeeFactory definition data with faker

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -8,21 +8,21 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class EmployeeFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
      * @var string
      */
     protected $model = Employee::class;
 
     /**
-     * Define the model's default state.
-     *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            //
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'company_id' => 1
         ];
     }
 }
