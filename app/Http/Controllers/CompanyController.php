5c9f0902535f5e88061f5cde050ef315094677e2
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 22:41:44 -0300

    Debug request in store method

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -26,16 +26,13 @@ class CompanyController extends Controller
         return view('admin.company.create');
     }
 
-
     /**
-     * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
