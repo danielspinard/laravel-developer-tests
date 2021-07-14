18c6fdad4696c71e0b84c1522a5fdee74e8b4355
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 22:30:18 -0300

    Add create company view

---
+++ resources/views/admin/company/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layout.app')
+
+@section('content')
+<div class="py-4"></div>
+
+<div class="card">
+    <div class="card-header">
+        Companies
+    </div>
+
+    <div class="card-body">
+        <a href="{{ route('company.index') }}" class="btn btn-primary">
+            Back to company list
+        </a>
+
+        <div class="card mt-3">
+            <div class="card-header">
+                Company info
+            </div>
+
+            <div class="card-body">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="py-4"></div>
+@endsection
+
+@push('style')
+    <style>
+        .card-header {
+            background-color: #fff;
+            padding: 12px 15px;
+        }
+    </style>
+@endpush
