ef88ee45efc0eaba31c2ba7bb8b54a00858f002c
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 21:33:38 -0300

    Add method to generate company website URL by your name

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -13,16 +13,27 @@ class CompanyFactory extends Factory
     protected $model = Company::class;
 
     /**
+     * @param string $name
+     * @return string
+     */
+    private function companyWebsite(string $name): string
+    {
+        return strtolower(explode(' ', $name)[1]) . '.com';
+    }
+
+    /**
      * @return array
      */
     public function definition(): array
     {
+        $name = $this->faker->name();
+
         return [
-            'name' => $this->faker->name(),
+            'name' => $name,
             'address' => $this->faker->address(),
             'email' => $this->faker->unique()->safeEmail(),
             'logo' => 'company.jpg',
-            'website' => 'website.com'
+            'website' => $this->companyWebsite($name)
         ];
     }
 }
