67f2da4d195d771c52dcd581d9b1aa87251f0a7f
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 22:34:26 -0300

    Add CompanyFactory and EmployeeFactory to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Company::factory(5)->create();
+        \App\Models\Employee::factory(50)->create();
+
         $this->call([
             UserSeeder::class
         ]);
