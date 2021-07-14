b655d7078f7f19bc25e1baa4dce317a4137baff5
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 22:25:56 -0300

    Add company.index route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/companies', [CompanyController::class, 'index'])->name('company.index');
