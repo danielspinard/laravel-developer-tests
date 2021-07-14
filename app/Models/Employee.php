668ff8b75f84958c247260900b246981e66883a0
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-13 21:45:23 -0300

    Add fillable fields

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'company_id'
+    ];
 }
