dea6f66aefb9efcdc66a7070b036e14cbee320d5
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-17 00:31:59 -0300

    Fix middleware auth to dashboard method

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,6 +7,14 @@ use Illuminate\View\View;
 class AppController extends Controller
 {
     /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['dashboard']);
+    }
+
+    /**
      * @return View
      */
     public function dashboard(): View
