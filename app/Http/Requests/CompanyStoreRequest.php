a0d9ac4ee8d74d8bf62b8617b1db1563b0edd840
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-17 00:49:17 -0300

    Add CompanyStoreRequest rules and update authorize method to true

--- app/Http/Requests/CompanyStoreRequest.php
+++ app/Http/Requests/CompanyStoreRequest.php
@@ -7,24 +7,24 @@ use Illuminate\Foundation\Http\FormRequest;
 class CompanyStoreRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|unique:companies',
+            'address' => 'min:3|max:255',
+            'website' => 'active_url',
+            'logo' => 'dimensions:min_width=100,min_height=100'
         ];
     }
 }
