b868994bf652eaf06112b5f0379e97e7a35df46b
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-20 16:19:55 -0300

    Remove unused method, show method render company info and form to edit

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -61,17 +61,6 @@ class CompanyController extends Controller
     }
     
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
