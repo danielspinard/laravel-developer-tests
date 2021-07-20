71a27b1b1dead399d10a81f31d970aefecc7df04
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-20 19:11:41 -0300

    Add method to send ajax store/update request with FormData

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -15,3 +15,42 @@ function getErrorsFromLaravelValidator(response) {
 
     return errors;
 }
+
+/**
+ * @param {HTMLFormElement} formElement
+ * @param {String} redirectOnSuccess 
+ */
+function ajaxStoreUpdateRequest(formElement, redirectOnSuccess) {
+    $.ajax({
+        url: formElement.attr("action"),
+        data: new FormData(formElement[0]),
+        method: "POST",
+        enctype: "multipart/form-data",
+        processData: false,
+        contentType: false,
+        success: (response) => {
+            let success = response.responseJSON;
+
+            return swal({
+                title: "Company created.",
+                text: "Company was created successfully.",
+                icon: "success",
+                button: false,
+                timer: 2000,
+            }).then(() => {
+                $(location).attr("href", redirectOnSuccess);
+            });
+        },
+        error: function (response) {
+            let error = response.responseJSON;
+            
+            return swal({
+                title: error.message,
+                text: getErrorsFromLaravelValidator(error),
+                icon: "error",
+                button: false,
+                timer: 3500,
+            });
+        }
+    });
+}
