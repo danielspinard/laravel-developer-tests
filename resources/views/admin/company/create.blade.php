4a9f8ccca59fafe6bc4b88bfe4aded823357f2a1
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-20 15:55:44 -0300

    Add sweetalert cdn

--- resources/views/admin/company/create.blade.php
+++ resources/views/admin/company/create.blade.php
@@ -8,17 +8,17 @@
         <div class="card-header">
             Companies
         </div>
-    
+
         <div class="card-body">
             <a href="{{ route('company.index') }}" class="btn btn-primary">
                 Back to company list
             </a>
-    
+
             <div class="card mt-3">
                 <div class="card-header">
                     Company info
                 </div>
-    
+
                 <div class="card-body">
                     @component('components.form')
                         @slot('route', route('company.store'))
@@ -30,3 +30,7 @@
     </div>
 </div>
 @endsection
+
+@push('script')
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+@endpush
