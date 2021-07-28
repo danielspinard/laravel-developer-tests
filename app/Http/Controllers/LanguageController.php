78f93a8095d83aa933f4824c8c8bb983e82441d2
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-28 19:30:35 -0300

    Fix method to change app locale by locale session

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests\LanguageUpdateRequest;
 
 class LanguageController extends Controller
 {
-    //
+    /**
+     * @param LanguageUpdateRequest $request
+     * @return RedirectResponse
+     */
+    public function change(LanguageUpdateRequest $request): RedirectResponse
+    {
+        App::setlocale($request->language);
+        Session::put('locale', $request->language);
+
+        return Redirect::back();
+    }
 }
