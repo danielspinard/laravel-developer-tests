0f952cd4a156fdd932b39de0d6df2e4eba4c064e
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-28 19:27:35 -0300

    Make Localization Middleware to set app locale

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class Localization
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale', 'en'));
+        }
+
+        return $next($request);
+    }
+}
