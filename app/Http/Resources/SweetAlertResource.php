dc3b2da923eb8605eb312a646f1c1e81f9399da2
Daniel Spinardi <55671450+danielspinard@users.noreply.github.com>
2021-07-31 16:21:37 -0300

    Add method to convert resource array to object and return on t aArray method

--- app/Http/Resources/SweetAlertResource.php
+++ app/Http/Resources/SweetAlertResource.php
@@ -7,13 +7,32 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class SweetAlertResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @const int
+     */
+    private const TIMER_CLOSE_MS = 3000;
+
+    /**
+     * @return object
+     */
+    private function resourceArrayToObject(): object
+    {
+        return (object) $this->resource;
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        $resource = $this->resourceArrayToObject();
+
+        return [
+            'title' => __($resource->title),
+            'text' => __($resource->text),
+            'icon' => $resource->icon,
+            'buttons' => false,
+            'timer' => SweetAlertResource::TIMER_CLOSE_MS
+        ];
     }
 }
